0a083a248044910bee2425a6b85bc0debf2fc7af
Dan Bikle <dan.bikle@gmail.com>
2014-11-29 08:38:54 +0000

    more

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'contact'   => 'posts#contact'
   get 'tags'      => 'posts#tags'
   get 'questions' => 'posts#questions'
-  get ':controller(/:action(/:id))'
+  get ':controller(/:action)'
 end
