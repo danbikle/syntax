9fd2a35a350d5bc033468c5c4a01d1c164af78d0
Dan Bikle <dan.bikle@gmail.com>
2014-12-01 06:03:14 +0000

    more

--- script/dirtags2haml.rb
+++ script/dirtags2haml.rb
@@ -3,4 +3,10 @@
 # I use this script to copy filenames from 
 # app/views/tags into app/views/posts/tags.haml
 
+require 'tempfile'
 
+tagdir   = "#{Rails.root}/app/views/tags"
+adir = Dir["#{tagdir}/_*.haml"]
+adir.each{ |fn|
+  p fn
+}
