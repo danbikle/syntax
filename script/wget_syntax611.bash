56687d7a247a686e57ba04da1a6a711881dd27dd
Dan Bikle <dan.bikle@gmail.com>
2014-12-27 23:02:04 +0000

    mo

--- script/wget_syntax611.bash
+++ script/wget_syntax611.bash
@@ -5,9 +5,22 @@
 set -x
 mkdir -p /tmp/wget_syntax/
 cd       /tmp/wget_syntax/
+
 wget www.syntax611.com
 wget www.syntax611.com/blog
-wget www.syntax611.com/posts
 wget www.syntax611.com/contact
+wget www.syntax611.com/posts
+wget www.syntax611.com/questions
+wget www.syntax611.com/tags
+
+mkdir -p /tmp/wget_syntax_us/
+cd       /tmp/wget_syntax_us/
+
+wget www.syntax.us
+wget www.syntax.us/blog
+wget www.syntax.us/contact
+wget www.syntax.us/posts
+wget www.syntax.us/questions
+wget www.syntax.us/tags
 
 exit
