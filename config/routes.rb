845cefa5152a655c8dcb6d5f4027acdbbd45ecc0
Dan Bikle <dan.bikle@gmail.com>
2015-02-12 07:24:52 +0000

    mo

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'blog'      => 'posts#blog'
   get 'contact'   => 'posts#contact'
   get 'tags'      => 'posts#tags'
+  get 'linux101'  => 'posts#linux101_cclud'
   get 'questions' => 'questions#index'
   get 'tags(/:tag)' => 'tags#tag'
   get ':controller(/:action)'
