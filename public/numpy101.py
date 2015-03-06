cc45f7b24e74391d6fa6f7b10b0cb2a770b777b6
Dan Bikle <dan.bikle@gmail.com>
2015-03-06 11:44:02 +0000

    mo

--- public/numpy101.py
+++ public/numpy101.py
@@ -76,13 +76,12 @@ cplag1 = cp +     [cp[-1]]
 cplag2 = cp +     [cp[-1]] + [cp[-1]]
 cplag4 = cp +     [cp[-1]] + [cp[-1]] + [cp[-1]] + [cp[-1]]
 cplag8 = cplag4 + [cp[-1]] + [cp[-1]] + [cp[-1]] + [cp[-1]]
-# I should snip off ends (1950s data which I do not use)
-# so new columns as long as cp:
-cplead = cplead[0:-1]
-cplag1 = cplag1[0:-1]
-cplag2 = cplag2[0:-2]
-cplag4 = cplag4[0:-4]
-cplag8 = cplag8[0:-8]
+# I should snip off ends so new columns as long as cp:
+cplead = cplead[:-1]
+cplag1 = cplag1[1:]
+cplag2 = cplag2[2:]
+cplag4 = cplag4[4:]
+cplag8 = cplag8[8:]
 # I should check new columns as long as cp:
 len(cp) == len(cplead)
 len(cp) == len(cplag4)
@@ -97,11 +96,11 @@ cplag4_a = np.array(cplag4)
 cplag8_a = np.array(cplag8)
 
 # I should calculate pct-deltas:
-pctlead_a = (cplead_a - cp_a)/cp_a
-pctlag1_a = (cp_a - cplag1_a)/cplag1_a
-pctlag2_a = (cp_a - cplag2_a)/cplag2_a
-pctlag4_a = (cp_a - cplag4_a)/cplag4_a
-pctlag8_a = (cp_a - cplag8_a)/cplag8_a
+pctlead_a = 100.0 * (cplead_a - cp_a)/cp_a
+pctlag1_a = 100.0 * (cp_a - cplag1_a)/cplag1_a
+pctlag2_a = 100.0 * (cp_a - cplag2_a)/cplag2_a
+pctlag4_a = 100.0 * (cp_a - cplag4_a)/cplag4_a
+pctlag8_a = 100.0 * (cp_a - cplag8_a)/cplag8_a
 
 # I am done doing calculations.
 # I should put my 5 new columns into my DataFrame.
@@ -111,7 +110,7 @@ df2['pctlag1'] = pctlag1_a
 df2['pctlag2'] = pctlag2_a
 df2['pctlag4'] = pctlag4_a
 df2['pctlag8'] = pctlag8_a
-
+pdb.set_trace()
 # I should save my work into a CSV file:
 df2.to_csv('numpy101.csv', float_format='%4.3f', index=False)
 
