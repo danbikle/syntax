2a030455f0c93d0d4dcfa9dc7a8f8e5a9c85db09
Dan Bikle <dan.bikle@gmail.com>
2016-01-09 00:32:20 +0000

    more

--- public/my.py
+++ public/my.py
@@ -1,9 +1,13 @@
-# cp_o5dmvg_avg.py
+# genf_demo.py
 
-# This script should demo how to create a list of observations of cp over 5day mvg avg.
+# This script should demo how to generate features from prices.
 
+# The syntax here comes mostly from this file:
+# https://github.com/danbikle/pyspy/blob/master/py/genf.py
+
+import numpy  as np
 import pdb
-import statistics
+
 
 # Here is some data:
 d_cp0_a = [
@@ -37,34 +41,5 @@ d_cp_a = [row for row in reversed(d_cp0_a)]
 # I should get the column of prices:
 cp_a = [row[1] for row in d_cp_a]
 
-# I should collect a moving avg:
-mvgavg5 = []
-for rn in range(1,1+len(cp_a)):
-  rn0 = rn-5
-  if rn0 < 0:
-    rn0 = 0
-  avgthis = cp_a[rn0:rn]
-  mvgavg5.append(statistics.mean(avgthis))
-
-# I should check my work:
-len(mvgavg5) == len(cp_a) # should be true
-mvgavg5[0]   == cp_a[0]   # should be true
-avgthis      == cp_a[-5+len(cp_a):len(cp_a)] # should be true
-avgthis      == [2060.98, 2056.5, 2078.36, 2063.36, 2043.94] # should be true
-mvgavg5[-1+len(cp_a)] == sum([2060.98, 2056.5, 2078.36, 2063.36, 2043.94])/5
-# should be true
-
-cp_o5dmvg_avg = []
-# I should add the feature now:
-for rn in range(0,len(cp_a)):
-  d_cp_a[rn].append(cp_a[rn]/mvgavg5[rn])
-
-# I should check my work:
-d_cp_a[0] == ['2015-12-01',2102.62,2102.62/2102.62] # should be true
-d_cp_a[1] == ['2015-12-02',2079.51,2079.51/((2079.51+2102.62)/2)] # should be true
-d_cp_a[len(cp_a)-1] == ['2015-12-31',2043.94,2043.94/(sum([2060.98, 2056.5, 2078.36, 2063.36, 2043.94])/5)] 
-# should be true
-
-print(d_cp_a)
-
+pdb.set_trace()
 'done'
